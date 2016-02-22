3f25cdfbcb36c355bee29f14b21d5c76cb4dca7a
Loo Juin <loojuin@gmail.com>
2016-02-22 18:01:34 +0800

    Found problem. Cleaned up code. Q2 fully functional.

--- week_3/q2.py
+++ week_3/q2.py
@@ -46,7 +46,6 @@ class DistanceMachine:
 			return self.struct[index]
 		except KeyError:
 			dist = compute_distance(self.error_matrix[:, a], self.error_matrix[:, b])
-			# print "Dist (%d, %d):" % (a, b), dist
 			self.struct[index] = dist
 			return dist
 
@@ -87,22 +86,22 @@ def neighbourhood_predict(actual_matrix, pred_matrix, size):
 				if error_matrix[u, n[0]] != -INFINITY_VALUE:
 					numerator += n[1] * error_matrix[u, n[0]]
 					denominator += np.abs(n[1])
-			offset = numerator/denominator
+			offset = numerator/denominator if denominator != 0 else 0
 			ret_matrix[u, i] = pred_matrix[u, i] + offset
 	return ret_matrix
 
 
 if __name__ == "__main__":
-	actual_matrix = np.matrix([[5, 4, 4, -1, 5],
-							   [-1, 3, 5, 3, 4],
-							   [5, 2, -1, 2, 3],
-							   [-1, 2, 3, 1, 2],
-							   [4, -1, 5, 4, 5],
-							   [5, 3, -1, 3, 5],
-							   [3, 2, 3, 2, -1],
-							   [5, 3, 4, -1, 5],
-							   [4, 2, 5, 4, -1],
-							   [5, -1, 5, 3, 4]])
+	actual_matrix = np.matrix([[5, 4, 4, -1, -1],
+							   [-1, 3, 5, -1, 4],
+							   [5, 2, -1, -1, 3],
+							   [-1, -1, 3, 1, 2],
+							   [4, -1, -1, 4, 5],
+							   [-1, 3, -1, 3, 5],
+							   [3, -1, 3, 2, -1],
+							   [5, -1, 4, -1, 5],
+							   [-1, 2, 5, 4, -1],
+							   [-1, -1, 5, 3, 4]])
 	print "Actual ratings:"
 	print actual_matrix
 	pred_matrix = np.matrix([[5.0, 3.09, 4.90, -1, 4.62],
